f03ec0783fb94656ff6635275fec4d4ccfc79820
Aleksander <al.ast8873@gmail.com>
2022-10-13 13:56:12 +0400

    2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,7 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarServisImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
@@ -20,7 +24,7 @@ public class CarController {
 
     @GetMapping()
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("cars",carServis.getListCar(count));
+        model.addAttribute("cars", carServis.getListCar(count));
         return "/cars";
     }
 
